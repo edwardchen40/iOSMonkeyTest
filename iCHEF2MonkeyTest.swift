062f67ae66676ad2093b940ced8906d08201bed5
Edward <edwardchen40git@gmail.com>
2016-12-02 16:47:30 +0800

    Update iCHEF2MonkeyTest.swift

--- iCHEF2MonkeyTest.swift
+++ iCHEF2MonkeyTest.swift
@@ -11,27 +11,31 @@ import XCTest
 class iCHEF2MonkeyTest: XCTestCase {
     
     let minimumGestureFrequency: UInt32 = 1 // Minimum amount of time to pass between gestures in seconds
-    let duration: Double = 60 * 120 // Execution time limit in seconds
-    let gestureLimit: UInt = 100 // Number of gestures to be executed
-    let gestureTypeCount: UInt32 = 14 // Number of types of gesture available
+    let duration: Double = 60 * 120 // Execution time limit in seconds (default 60 * 120)
+    let gestureLimit: UInt = 100 // Number of gestures to be executed (default 100)
+    let gestureTypeCount: UInt32 = 12 // Number of types of gesture available
     let app = XCUIApplication()
-    
+
     override func setUp() {
         super.setUp()
         // In these chaos tests, why not continue after failure, if nothing's crashed? CHAOS!
         continueAfterFailure = true
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        
+        // To take a snapshot
+        setupSnapshot(app)
         app.launch()
         
         let passwordSecureTextField = app.secureTextFields["Password"]
         passwordSecureTextField.tap()
         passwordSecureTextField.typeText("admin")
+        snapshot("Enter password")
         app.images["LoginView-iCHEFLogo"].tap()
         app.buttons["loginButton"].tap()
         
         
         waitForElementToAppear(element: app.buttons["B4_addButton"], timeout: 20)
-
+        snapshot("Login successful")
     }
     
     override func tearDown() {
@@ -55,19 +59,6 @@ class iCHEF2MonkeyTest: XCTestCase {
         }
     }
     
-    func waitForElementToAppear(element: XCUIElement, timeout: TimeInterval = 5,  file: String = #file, line: UInt = #line) {
-        let existsPredicate = NSPredicate(format: "exists == true")
-        
-        expectation(for: existsPredicate, evaluatedWith: element, handler: nil)
-        
-        waitForExpectations(timeout: timeout) { (error) -> Void in
-            if (error != nil) {
-                let message = "Failed to find \(element) after \(timeout) seconds."
-                self.recordFailure(withDescription: message, inFile: file, atLine: line, expected: true)
-            }
-        }
-    }
-
     
     private func executeRandomGesture() {
         let randomGestureID = arc4random_uniform(gestureTypeCount)
@@ -76,8 +67,12 @@ class iCHEF2MonkeyTest: XCTestCase {
         switch randomGestureID {
         case 0:
             coordinate.tap()
+            snapshot("Tap location on \(coordinate)")
+            
         case 1:
             coordinate.doubleTap()
+            snapshot("Double tap location on \(coordinate)")
+        
         case 2:
             // Scroll up
             let maxSize = app.windows.element(boundBy: 0).frame.size
@@ -85,6 +80,9 @@ class iCHEF2MonkeyTest: XCTestCase {
             let dy = (startY * getRandomValueBetween0And1()) / maxSize.height
             let vector = CGVector(dx: coordinate.screenPoint.x / maxSize.width, dy: dy)
             scroll(fromCoordinate: coordinate, toCoordinate: getCoordinateForVector(vector: vector))
+            snapshot("Scroll up from \(coordinate) to \(vector)")
+            coordinate.tap()
+            
         case 3:
             // Scroll down
             let maxSize = app.windows.element(boundBy: 0).frame.size
@@ -92,6 +90,9 @@ class iCHEF2MonkeyTest: XCTestCase {
             let dy = ((maxSize.height - startY) * getRandomValueBetween0And1() + startY) / maxSize.height
             let vector = CGVector(dx: coordinate.screenPoint.x / maxSize.width, dy: dy)
             scroll(fromCoordinate: coordinate, toCoordinate: getCoordinateForVector(vector: vector))
+            snapshot("Scroll down from \(coordinate) to \(vector)")
+            coordinate.tap()
+            
         case 4:
             // Scroll left
             let maxSize = app.windows.element(boundBy: 0).frame.size
@@ -99,6 +100,9 @@ class iCHEF2MonkeyTest: XCTestCase {
             let dx = (startX * getRandomValueBetween0And1()) / maxSize.width
             let vector = CGVector(dx: dx, dy: coordinate.screenPoint.y / maxSize.height)
             scroll(fromCoordinate: coordinate, toCoordinate: getCoordinateForVector(vector: vector))
+            snapshot("Scroll left from \(coordinate) to \(vector)")
+            coordinate.tap()
+            
         case 5:
             // Scroll right
             let maxSize = app.windows.element(boundBy: 0).frame.size
@@ -106,23 +110,41 @@ class iCHEF2MonkeyTest: XCTestCase {
             let dx = ((maxSize.width - startX) * getRandomValueBetween0And1() + startX) / maxSize.width
             let vector = CGVector(dx: dx, dy: coordinate.screenPoint.y / maxSize.height)
             scroll(fromCoordinate: coordinate, toCoordinate: getCoordinateForVector(vector: vector))
+            snapshot("Scroll right from \(coordinate) to \(vector)")
+            coordinate.tap()
+            
         case 6:
             device.orientation = .portrait
+            snapshot("Orientation -> Portrait")
+            coordinate.tap()
+            
         case 7:
             device.orientation = .portraitUpsideDown
+            snapshot("Orientation -> PortraitUpsideDown")
+            coordinate.tap()
+            
         case 8:
             device.orientation = .landscapeLeft
+            snapshot("Orientation -> LandscapeLeft")
+            coordinate.tap()
+            
         case 9:
             device.orientation = .landscapeRight
+            snapshot("Orientation -> LandscapeRight")
+            coordinate.tap()
+      //      device.orientation = .faceUp
+      //  case 11:
+      //      device.orientation = .faceDown
+            
         case 10:
-            device.orientation = .faceUp
-        case 11:
-            device.orientation = .faceDown
-        case 12:
             // Move in a completely random direction
             scroll(fromCoordinate: coordinate, toCoordinate: getRandomCoordinate())
-        case 13:
+            snapshot("Scroll random direction")
+            coordinate.tap()
+            
+        case 11:
             coordinate.press(forDuration: 2)
+            
         default:
             XCTFail("Random number failure - unhandled case for number: \(randomGestureID)")
         }
@@ -130,7 +152,9 @@ class iCHEF2MonkeyTest: XCTestCase {
         
         // Wait for cooldown period
         sleep(minimumGestureFrequency)
+        print("Memory (MB) =", getMegabytesUsed())
     }
+    
     
     private func getRandomCoordinate() -> XCUICoordinate {
         let randomX = getRandomValueBetween0And1()
@@ -155,4 +179,42 @@ class iCHEF2MonkeyTest: XCTestCase {
     private func scroll(fromCoordinate: XCUICoordinate, toCoordinate: XCUICoordinate) {
         fromCoordinate.press(forDuration: 0, thenDragTo: toCoordinate)
     }
+    
+    func waitForElementToAppear(element: XCUIElement, timeout: TimeInterval = 5,  file: String = #file, line: UInt = #line) {
+        let existsPredicate = NSPredicate(format: "exists == true")
+        
+        expectation(for: existsPredicate, evaluatedWith: element, handler: nil)
+        
+        waitForExpectations(timeout: timeout) { (error) -> Void in
+            if (error != nil) {
+                let message = "Failed to find \(element) after \(timeout) seconds."
+                self.recordFailure(withDescription: message, inFile: file, atLine: line, expected: true)
+            }
+        }
+    }
+    
+    // Get Memory Usage (How many memory was used by MB)
+    func mach_task_self() -> task_t {
+        return mach_task_self_
+    }
+    
+    func getMegabytesUsed() -> Float? {
+        var info = mach_task_basic_info()
+        var count = mach_msg_type_number_t(MemoryLayout.size(ofValue: info) / MemoryLayout<integer_t>.size)
+        let kerr = withUnsafeMutablePointer(to: &info) { infoPtr in
+            return infoPtr.withMemoryRebound(to: integer_t.self, capacity: Int(count)) { (machPtr: UnsafeMutablePointer<integer_t>) in
+                return task_info(
+                    mach_task_self(),
+                    task_flavor_t(MACH_TASK_BASIC_INFO),
+                    machPtr,
+                    &count
+                )
+            }
+        }
+        guard kerr == KERN_SUCCESS else {
+            return nil
+        }
+        return Float(info.resident_size) / (1024 * 1024)
+    }
+    
 }
